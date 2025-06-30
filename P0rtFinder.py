cea541a365c4212603538e5ee1e77ef7b7f27c87
AYUSH KUMAR <161255870+AyushKr41@users.noreply.github.com>
2025-06-30 06:10:18 +0530

    Updated

--- P0rtFinder.py
+++ P0rtFinder.py
@@ -74,6 +74,7 @@ def print_banner():
     print(f"{GREEN}{banner}{RESET}")
     print(f"Developed by Ayush Kumar")
     print(f"GitHub: https://github.com/AyushKr41")
+    print(f"Linkedin: https://www.linkedin.com/in/ayush-kumar-ak4422")
     print(f"\nP0rtFinder is an advanced port scanner that helps you identify open ports and potential vulnerabilities now powered with the NSE .\n")
 
 def main(target, ports_range, protocol, nse_scripts=None, verbose=False):
